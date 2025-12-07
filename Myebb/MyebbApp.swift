d34892bedd40701a78ec80e8d54b8fcaae5f7420
Eric Almendral <eralmndrx@gmail.com>
2025-12-07 11:14:33 +0800

    Initial Commit

---
+++ Myebb/MyebbApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyebbApp.swift
+//  Myebb
+//
+//  Created by Eric Al on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MyebbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyebbTests/MyebbTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyebbTests.swift
+//  MyebbTests
+//
+//  Created by Eric Al on 12/7/25.
+//
+
+import Testing
+@testable import Myebb
+
+struct MyebbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
